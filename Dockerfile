140235a6cc894db43bc3f85c2db71785397bee6d
njokuifeanyigerald <brainbboyrichmond@gmail.com>
2021-12-05 08:09:31 +0100

    second commit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,22 @@
 FROM python:3
 
-# 
-ENV PYTHONBUFFERED=1
+# to make sure everything is nicely accesible
+ENV PYTHONUNBUFFERED=1
 
 # where the app will be held
-WORKDIR /app
+WORKDIR /aftermath
 
-
+# copy the installed files in the txt file
 COPY requirements.txt requirements.txt
+
+# to make sure we re using the latest pip 
+RUN pip3 install --upgrade pip
+
+# install files in requirements.txt
+RUN  pip3 install -r requirements.txt
+
+# everything across
+COPY . /aftermath
+
+# port to use it
+EXPOSE 8000
--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware, db
 
-app = FastAPI
+from schema import Book as SchemaBook
+from schema import Author as SchemaAuthor
+
+from schema import Book
+from schema import Author
+
+from models import Book as ModelBook
+from models import Author as ModelAuthor
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+
+app = FastAPI()
+
+# to avoid csrftokenError
+app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.post('/add_book/', response_model=SchemaBook)
+async def book(book: SchemaBook):
+    db_book = ModelBook(title=book.title, rating=book.rating, author_id = book.author_id)
+    db.session.add(db_book)
+    db.session.commit()
+    return db_book
+
+@app.post('/author/', response_model=SchemaAuthor)
+async def author(author:SchemaAuthor):
+    db_author = ModelAuthor(name=author.name, age=author.age)
+    db.session.add(db_author)
+    db.session.commit()
+    return db_author
